78a2c505c288cf615278e9841ee0a20c3c72f4ee
Bilguun Baasansuren <bill@Bilguuns-MacBook-Pro.local>
2025-12-06 16:58:27 +0800

    shadcn pagination component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -14,6 +14,7 @@ export * from "./components/ui/dropdown-menu";
 export * from "./components/ui/form";
 export * from "./components/ui/input";
 export * from "./components/ui/label";
+export * from "./components/ui/pagination";
 export * from "./components/ui/resizable";
 export * from "./components/ui/select";
 export * from "./components/ui/sonner";
