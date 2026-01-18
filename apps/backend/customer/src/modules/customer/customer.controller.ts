70d271c181260f78074ca08b431a49270a48521c
daze <bilguun889@gmail.com>
2026-01-18 16:06:06 +0800

    test

--- apps/backend/customer/src/modules/customer/customer.controller.ts
+++ apps/backend/customer/src/modules/customer/customer.controller.ts
@@ -32,6 +32,7 @@ export class CustomerController {
       customerContract.customer.updateCustomerProfile,
       async ({ body }) => {
         await this.customerService.updateCustomerProfile(body);
+        console.log(body, "body");
 
         return {
           status: 200,
